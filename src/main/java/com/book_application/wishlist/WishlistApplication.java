5d6784fe042cbc4d8c356e9f8f94c0fb97b70ade
AntonioGlezCanti <gonzalezantoniolfm@gmail.com>
2025-09-21 21:21:12 +0200

    exampleController added

--- src/main/java/com/book_application/wishlist/WishlistApplication.java
+++ src/main/java/com/book_application/wishlist/WishlistApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WishlistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WishlistApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(WishlistApplication.class, args);
+  }
 }
